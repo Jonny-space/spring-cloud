f81e67c1e1f95014f87cf233fd79b580af8e4e87
Jonny-space <m13830711129_1@163.com>
2019-01-18 18:07:06 +0800

    删除

--- springcloud-eureka-server/src/main/java/com/jonny/spring/eureka/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jonny.spring.eureka.repository;
-
-import com.jonny.spring.eureka.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department,Long> {
-}
