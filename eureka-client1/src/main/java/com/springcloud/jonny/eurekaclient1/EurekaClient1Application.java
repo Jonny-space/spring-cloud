6b7e7df937a72de0ac6e5ec10a6751dfa1ccef5b
Jonny-space <m13830711129_1@163.com>
2019-01-19 18:06:37 +0800

    version 1

--- eureka-client1/src/main/java/com/springcloud/jonny/eurekaclient1/EurekaClient1Application.java
+++ eureka-client1/src/main/java/com/springcloud/jonny/eurekaclient1/EurekaClient1Application.java
@@ -2,8 +2,11 @@ package com.springcloud.jonny.eurekaclient1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,6 +14,5 @@ public class EurekaClient1Application {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClient1Application.class, args);
     }
-
 }
 
--- service-feign/src/main/java/com/springcloud/jonny/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/springcloud/jonny/servicefeign/ServiceFeignApplication.java
@@ -3,7 +3,10 @@ package com.springcloud.jonny.servicefeign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +16,10 @@ public class ServiceFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceFeignApplication.class, args);
     }
-
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return  new RestTemplate();
+    }
 }
 
